f59e872e49959bb7e77d7c69aedacb34b859d628
Eden Ottley-Hodgson <lh12g22@soton.ac.uk>
2023-07-24 01:12:06 +0100

    Added TS language server

--- init.lua
+++ init.lua
@@ -166,6 +166,8 @@ require'lspconfig'.clangd.setup{}
 require'lspconfig'.jdtls.setup{}
 require'lspconfig'.texlab.setup{}
 require'lspconfig'.pylsp.setup{}
+require'lspconfig'.tsserver.setup{}
+
 require'sad'.setup({
   debug = false, -- print debug info
   diff = 'delta', -- you can use `less`, `diff-so-fancy`
